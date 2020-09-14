f947bf789b2efa3d503f03bc272ca4a012d8ecf4
Matthias McCarthy <matthias.mccarthy@gmail.com>
2020-09-14 16:54:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", dice1);
 
-
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 
 var randomDiceImage2 = `dice${randomNumber2}.png`;
